95e50d9174a8c5d3ee9a6cfc152f40fc2ee3ec25
Joao Augusto <frajolacommel@gmail.com>
2020-11-24 18:54:11 -0300

    concluido

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+       {
+            int nota ;
+            do {
+            Console.Write("digite um numero entre 0 - 10 : ");
+                 nota = int.Parse(Console.ReadLine());
+
+                if(nota <0 || nota >10){
+                    Console.WriteLine("algarismo inválido");
+                }
+        } 
+        
+            while(nota <0 || nota >10);
+
+            Console.WriteLine("algorismo valido!!");
+        }
+    }
+}
